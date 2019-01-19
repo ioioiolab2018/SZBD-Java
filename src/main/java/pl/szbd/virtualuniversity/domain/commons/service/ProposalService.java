a19dc0d37731a68d07d7761df081917edad50413
Stapior <sensaj10@gmail.com>
2019-01-19 19:40:13 +0100

    Poprawka wniosków

--- src/main/java/pl/szbd/virtualuniversity/domain/commons/service/ProposalService.java
+++ src/main/java/pl/szbd/virtualuniversity/domain/commons/service/ProposalService.java
@@ -3,6 +3,7 @@ package pl.szbd.virtualuniversity.domain.commons.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.szbd.virtualuniversity.domain.commons.model.TableData;
+import pl.szbd.virtualuniversity.domain.commons.model.entities.Person;
 import pl.szbd.virtualuniversity.domain.commons.model.entities.Proposal;
 import pl.szbd.virtualuniversity.domain.commons.model.entities.Student;
 import pl.szbd.virtualuniversity.domain.commons.model.entities.User;
@@ -20,6 +21,8 @@ public class ProposalService {
     private StudentService studentService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private PersonService personService;
 
     public List<TableData> getProposals(String username) {
         User user = userService.getUserByUsername(username);
@@ -43,13 +46,14 @@ public class ProposalService {
 
         if (student != null) {
             List<TableData> result;
+            String user = personService.getPerson(student.getPersonId()).getName() + " " + personService.getPerson(student.getPersonId()).getSurname() +" "+ index;
             if (answer.equals("false")) {
                 result = proposalRepository.getProposalsByPersonId(student.getPersonId())
                         .stream().filter(proposal -> proposal.getAnswerDate() == null).map(element -> new TableData(
                                 element.getId(),
                                 element.getTopic(),
+                                user,
                                 DateFormatter.getFormatter().format(element.getSubmissionDate()),
-                                element.getAnswerDate() != null ? DateFormatter.getFormatter().format(element.getAnswerDate()) : "",
                                 element.getShortAnswer() != null ? element.getShortAnswer() : ""))
                         .collect(Collectors.toList());
                 return result.subList(0, Math.min(9, result.size() - 1));
@@ -59,8 +63,8 @@ public class ProposalService {
                         .stream().filter(proposal -> proposal.getAnswerDate() != null).map(element -> new TableData(
                                 element.getId(),
                                 element.getTopic(),
+                                user,
                                 DateFormatter.getFormatter().format(element.getSubmissionDate()),
-                                element.getAnswerDate() != null ? DateFormatter.getFormatter().format(element.getAnswerDate()) : "",
                                 element.getShortAnswer() != null ? element.getShortAnswer() : ""))
                         .collect(Collectors.toList());
                 return result.subList(0, Math.min(9, result.size() - 1));
